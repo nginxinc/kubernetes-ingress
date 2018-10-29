81843c85eca679b0ed6375c3083009c129211431
Dean Coakley <dean.s.coakley@gmail.com>
2018-10-29 19:55:32 +0000

    Update nginx_test.go

--- internal/nginx/nginx_test.go
+++ internal/nginx/nginx_test.go
@@ -13,12 +13,12 @@ func TestGetNginxCommand(t *testing.T) {
 		{"reload", "/usr/sbin/nginx", "/usr/sbin/nginx -s reload"},
 		{"stop", "/usr/sbin/nginx-debug", "/usr/sbin/nginx-debug -s stop"},
 	}
-	for _, tt := range tests {
-		t.Run(tt.cmd, func(t *testing.T) {
-			nginx := NewNginxController("/etc/nginx", tt.nginxBinaryPath, true)
+	for _, test := range tests {
+		t.Run(test.cmd, func(t *testing.T) {
+			nginx := NewNginxController("/etc/nginx", test.nginxBinaryPath, true)
 
-			if got := nginx.getNginxCommand(tt.cmd); got != tt.expected {
-				t.Errorf("getNginxCommand returned \n%v, but expected \n%v", got, tt.expected)
+			if got := nginx.getNginxCommand(test.cmd); got != test.expected {
+				t.Errorf("getNginxCommand returned \n%v, but expected \n%v", got, test.expected)
 			}
 		})
 	}
