c48326c68087ceb29eb9b6df058553c3e9f27bc6
LorcanMcVeigh <lorcan.mcveigh@gmail.com>
2020-06-03 13:23:06 +0100

    Fix regex error in release scripts

--- hack/prepare-minor-release-in-master.sh
+++ hack/prepare-minor-release-in-master.sh
@@ -18,8 +18,8 @@ fi
 ic_version=$1
 helm_chart_version=$2
 
-prev_ic_version=$(echo $ic_version | awk -F. '{ printf("%s.%s.%d", $1, $2, $3-1) }')
-prev_helm_chart_version=$(echo $helm_chart_version | awk -F. '{ printf("%s.%s.%d", $1, $2, $3-1) }')
+prev_ic_version=$(echo $ic_version | awk -F. '{ printf("%s\\.%s\\.%d", $1, $2, $3-1) }')
+prev_helm_chart_version=$(echo $helm_chart_version | awk -F. '{ printf("%s\\.%s\\.%d", $1, $2, $3-1) }')
 
 sed -i "" "s/$prev_ic_version/$ic_version/g" README.md
 
