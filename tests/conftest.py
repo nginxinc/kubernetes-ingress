1157a02000b5db6123f4a046aba2facecc3dbd82
Sean O'Neill <s.oneill@f5.com>
2021-04-14 11:37:38 +0100

    Changed to only update apiversion

--- tests/conftest.py
+++ tests/conftest.py
@@ -98,11 +98,11 @@ def pytest_collection_modifyitems(config, items) -> None:
         for item in items:
             if "skip_for_nginx_plus" in item.keywords:
                 item.add_marker(skip_for_nginx_plus)
-    if "-ap" not in config.getoption("--image"):
-        appprotect = pytest.mark.skip(reason="Skip AppProtect test in non-AP image")
-        for item in items:
-            if "appprotect" in item.keywords:
-                item.add_marker(appprotect)
+    # if "-ap" not in config.getoption("--image"):
+    #     appprotect = pytest.mark.skip(reason="Skip AppProtect test in non-AP image")
+    #     for item in items:
+    #         if "appprotect" in item.keywords:
+    #             item.add_marker(appprotect)
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
