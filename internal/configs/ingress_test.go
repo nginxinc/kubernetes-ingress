fb8bbd44c788ff0f057c6263d72354c5f63d4f25
= <=>
2021-09-30 16:53:47 +0300

    fix duplicate functions from merge

--- internal/configs/ingress_test.go
+++ internal/configs/ingress_test.go
@@ -1471,7 +1471,7 @@ func TestGenerateNginxCfgForAppProtectDos(t *testing.T) {
 	}
 }
 
-func TestGenerateNginxCfgForMergeableIngressesForAppProtectDos(t *testing.T) {
+func TestGenerateNginxCfgForMergeableIngressesForAppProtectDos2(t *testing.T) {
 	mergeableIngresses := createMergeableCafeIngress()
 	mergeableIngresses.Master.Ingress.Annotations["appprotectdos.f5.com/app-protect-dos-enable"] = "True"
 	mergeableIngresses.Master.Ingress.Annotations["appprotectdos.f5.com/app-protect-dos-security-log-enable"] = "True"
