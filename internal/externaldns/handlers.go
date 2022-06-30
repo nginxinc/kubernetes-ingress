1951dfdb7c3e26223bfeeef192760ef3cfbd3ce7
Ciara Stacke <c.stacke@f5.com>
2022-06-30 12:37:26 +0100

    Remove unused function

--- internal/externaldns/handlers.go
+++ internal/externaldns/handlers.go
@@ -4,15 +4,9 @@ import (
 	"reflect"
 	"time"
 
-	"github.com/go-logr/logr"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
-
-	logf "github.com/cert-manager/cert-manager/pkg/logs"
 )
 
 // KeyFunc creates a key for an API object. The key can be passed to a
@@ -24,60 +18,6 @@ var KeyFunc = cache.DeletionHandlingMetaNamespaceKeyFunc
 // seconds, max delay of 5 minutes.
 func DefaultItemBasedRateLimiter() workqueue.RateLimiter {
 	return workqueue.NewItemExponentialFailureRateLimiter(time.Second*5, time.Minute*5)
-}
-
-// HandleOwnedResourceNamespacedFunc returns a function thataccepts a
-// Kubernetes object and adds its owner references to the workqueue.
-// https://kubernetes.io/docs/concepts/workloads/controllers/garbage-collection/#owners-and-dependents
-func HandleOwnedResourceNamespacedFunc(log logr.Logger, queue workqueue.RateLimitingInterface, ownerGVK schema.GroupVersionKind, get func(namespace, name string) (interface{}, error)) func(obj interface{}) {
-	return func(obj interface{}) {
-		log := log.WithName("handleOwnedResource")
-
-		metaobj, ok := obj.(metav1.Object)
-		if !ok {
-			log.Error(nil, "item passed to handleOwnedResource does not implement metav1.Object")
-			return
-		}
-		log = logf.WithResource(log, metaobj)
-
-		ownerRefs := metaobj.GetOwnerReferences()
-		for _, ref := range ownerRefs {
-			log := log.WithValues(
-				logf.RelatedResourceNamespaceKey, metaobj.GetNamespace(),
-				logf.RelatedResourceNameKey, ref.Name,
-				logf.RelatedResourceKindKey, ref.Kind,
-			)
-
-			// Parse the Group out of the OwnerReference to compare it to what was parsed out of the requested OwnerType
-			refGV, err := schema.ParseGroupVersion(ref.APIVersion)
-			if err != nil {
-				log.Error(err, "could not parse OwnerReference GroupVersion")
-				continue
-			}
-
-			if refGV.Group == ownerGVK.Group && ref.Kind == ownerGVK.Kind {
-				obj, err := get(metaobj.GetNamespace(), ref.Name)
-				// This function is always called with a getter
-				// that gets from informers cache. Because this
-				// is also called on cache sync it may be that
-				// the owner is not yet in the cache.
-				if err != nil && errors.IsNotFound(err) {
-					log.Info("owning resource not found in cache")
-					continue
-				}
-				if err != nil {
-					log.Error(err, "error getting referenced owning resource from cache")
-					continue
-				}
-				objKey, err := KeyFunc(obj)
-				if err != nil {
-					log.Error(err, "error computing key for resource")
-					continue
-				}
-				queue.Add(objKey)
-			}
-		}
-	}
 }
 
 // QueuingEventHandler is an implementation of cache.ResourceEventHandler that
