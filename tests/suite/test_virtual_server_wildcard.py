29a19b90b5daf4b8cb073bd5de32d07921362353
Venktesh <ve.patel@f5.com>
2022-08-24 13:32:37 +0100

    lint and retry for vs status

--- tests/suite/test_virtual_server_wildcard.py
+++ tests/suite/test_virtual_server_wildcard.py
@@ -1,13 +1,10 @@
 import pytest
-import requests
 from settings import TEST_DATA
 from suite.custom_assertions import wait_and_assert_status_code
 from suite.custom_resources_utils import read_custom_resource
-from suite.vs_vsr_resources_utils import (
-    create_virtual_server_from_yaml,
-    delete_virtual_server,
-)
 from suite.resources_utils import wait_before_test
+from suite.vs_vsr_resources_utils import create_virtual_server_from_yaml, delete_virtual_server
+
 
 @pytest.mark.vs
 @pytest.mark.parametrize(
@@ -21,9 +18,8 @@ from suite.resources_utils import wait_before_test
     indirect=True,
 )
 class TestVirtualServerWildcard:
-
     def test_vs_status(self, kube_apis, crd_ingress_controller, virtual_server_setup):
-        
+
         wait_and_assert_status_code(200, virtual_server_setup.backend_1_url, virtual_server_setup.vs_host)
         wait_and_assert_status_code(200, virtual_server_setup.backend_2_url, virtual_server_setup.vs_host)
         wait_and_assert_status_code(404, virtual_server_setup.backend_1_url, "test.example.com")
@@ -31,7 +27,9 @@ class TestVirtualServerWildcard:
 
         # create virtual server with wildcard hostname
         manifest_vs_wc = f"{TEST_DATA}/virtual-server-wildcard/virtual-server-wildcard.yaml"
-        vs_wc_name = create_virtual_server_from_yaml(kube_apis.custom_objects, manifest_vs_wc, virtual_server_setup.namespace)
+        vs_wc_name = create_virtual_server_from_yaml(
+            kube_apis.custom_objects, manifest_vs_wc, virtual_server_setup.namespace
+        )
         wait_before_test()
         response = read_custom_resource(
             kube_apis.custom_objects,
@@ -39,11 +37,15 @@ class TestVirtualServerWildcard:
             "virtualservers",
             vs_wc_name,
         )
-        assert (
-            response["status"]
-            and response["status"]["reason"] == "AddedOrUpdated"
-            and response["status"]["state"] == "Valid"
-        )
+        while not response["status"]:
+            response = read_custom_resource(
+                kube_apis.custom_objects,
+                virtual_server_setup.namespace,
+                "virtualservers",
+                vs_wc_name,
+            )
+
+        assert response["status"]["reason"] == "AddedOrUpdated" and response["status"]["state"] == "Valid"
         wait_and_assert_status_code(200, virtual_server_setup.backend_1_url, "test.example.com")
         wait_and_assert_status_code(200, virtual_server_setup.backend_2_url, "test.example.com")
         wait_and_assert_status_code(404, virtual_server_setup.backend_1_url, "test.xexample.com")
