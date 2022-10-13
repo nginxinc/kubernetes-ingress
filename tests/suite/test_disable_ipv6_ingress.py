66899e0e9a42bb854eca4866af17aa0945b9bbec
Haywood Shannon <5781935+haywoodsh@users.noreply.github.com>
2022-10-13 12:34:49 +0100

    Remove unnecessary files and variables

--- tests/suite/test_disable_ipv6_ingress.py
+++ tests/suite/test_disable_ipv6_ingress.py
@@ -1,6 +1,6 @@
 import pytest
+
 from settings import TEST_DATA
-from suite.fixtures import PublicEndpoint
 from suite.resources_utils import (
     create_example_app,
     create_items_from_yaml,
@@ -15,52 +15,38 @@ from suite.resources_utils import (
     wait_before_test,
     wait_until_all_pods_are_ready,
 )
-from suite.yaml_utils import get_first_ingress_host_from_yaml, get_name_from_yaml
-
-paths = ["backend1", "backend2"]
+from suite.yaml_utils import get_name_from_yaml
 
 
-class DisableIPV6Setup:
+class IngressSetup:
     """
     Encapsulate the Disable IPV6 Example details.
 
     Attributes:
-        public_endpoint (PublicEndpoint):
         ingress_name (str):
-        ingress_host (str):
         ingress_pod_name (str):
         namespace (str):
     """
 
-    def __init__(self, public_endpoint: PublicEndpoint, ingress_name, ingress_host, ingress_pod_name, namespace):
-        self.public_endpoint = public_endpoint
-        self.ingress_host = ingress_host
+    def __init__(self, ingress_name, ingress_pod_name, namespace):
         self.ingress_name = ingress_name
         self.ingress_pod_name = ingress_pod_name
         self.namespace = namespace
 
 
-@pytest.fixture(scope="class", params=["standard", "mergeable"])
-def disable_ipv6_setup(
-    request,
-    kube_apis,
-    ingress_controller_prerequisites,
-    ingress_controller_endpoint,
-    ingress_controller,
-    test_namespace,
-) -> DisableIPV6Setup:
+@pytest.fixture(scope="class")
+def ingress_setup(
+        request,
+        kube_apis,
+        ingress_controller_prerequisites,
+        ingress_controller_endpoint,
+        ingress_controller,
+        test_namespace,
+) -> IngressSetup:
     print("------------------------- Deploy Disable IPV6 Example -----------------------------------")
-    secret_name = create_secret_from_yaml(
-        kube_apis.v1, test_namespace, f"{TEST_DATA}/disable-ipv6-ingress/disable-ipv6-secret.yaml"
-    )
-
-    create_items_from_yaml(
-        kube_apis, f"{TEST_DATA}/disable-ipv6-ingress/{request.param}/disable-ipv6-ingress.yaml", test_namespace
-    )
-    ingress_name = get_name_from_yaml(f"{TEST_DATA}/disable-ipv6-ingress/{request.param}/disable-ipv6-ingress.yaml")
-    ingress_host = get_first_ingress_host_from_yaml(
-        f"{TEST_DATA}/disable-ipv6-ingress/{request.param}/disable-ipv6-ingress.yaml"
-    )
+    secret_name = create_secret_from_yaml(kube_apis.v1, test_namespace, f"{TEST_DATA}/smoke/smoke-secret.yaml")
+    create_items_from_yaml(kube_apis, f"{TEST_DATA}/smoke/standard/smoke-ingress.yaml", test_namespace)
+    ingress_name = get_name_from_yaml(f"{TEST_DATA}/smoke/standard/smoke-ingress.yaml")
     create_example_app(kube_apis, "simple", test_namespace)
     wait_until_all_pods_are_ready(kube_apis.v1, test_namespace)
 
@@ -74,14 +60,12 @@ def disable_ipv6_setup(
     def fin():
         print("Clean up the Disable IPV6 Application:")
         delete_common_app(kube_apis, "simple", test_namespace)
-        delete_items_from_yaml(
-            kube_apis, f"{TEST_DATA}/disable-ipv6-ingress/{request.param}/disable-ipv6-ingress.yaml", test_namespace
-        )
+        delete_items_from_yaml(kube_apis, f"{TEST_DATA}/smoke/standard/smoke-ingress.yaml", test_namespace)
         delete_secret(kube_apis.v1, secret_name, test_namespace)
 
     request.addfinalizer(fin)
 
-    return DisableIPV6Setup(ingress_controller_endpoint, ingress_name, ingress_host, ic_pod_name, test_namespace)
+    return IngressSetup(ingress_name, ic_pod_name, test_namespace)
 
 
 @pytest.mark.ingresses
@@ -89,23 +73,25 @@ class TestDisableIPV6:
     @pytest.mark.parametrize(
         "ingress_controller",
         [
-            pytest.param({"extra_args": ["-disable-ipv6"]}, id="one-additional-cli-args"),
+            pytest.param({"extra_args": ["-disable-ipv6"]}),
         ],
         indirect=True,
     )
     def test_ipv6_listeners_not_in_config(
-        self,
-        kube_apis,
-        disable_ipv6_setup: DisableIPV6Setup,
-        ingress_controller_prerequisites,
+            self,
+            kube_apis,
+            ingress_setup: IngressSetup,
+            ingress_controller_prerequisites,
     ):
         wait_before_test()
-        nginx_config = get_nginx_template_conf(kube_apis.v1, ingress_controller_prerequisites.namespace)
+        nginx_config = get_nginx_template_conf(
+            kube_apis.v1, ingress_controller_prerequisites.namespace, ingress_setup.ingress_pod_name
+        )
         upstream_conf = get_ingress_nginx_template_conf(
             kube_apis.v1,
-            disable_ipv6_setup.namespace,
-            disable_ipv6_setup.ingress_name,
-            disable_ipv6_setup.ingress_pod_name,
+            ingress_setup.namespace,
+            ingress_setup.ingress_name,
+            ingress_setup.ingress_pod_name,
             ingress_controller_prerequisites.namespace,
         )
         assert "listen [::]:" not in nginx_config
