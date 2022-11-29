d93c00765f1058235f783efa5c5977d90b47fce7
Venktesh <ve.patel@f5.com>
2022-11-29 17:26:19 +0000

    Add test for https endpoint

--- tests/suite/test_healthcheck_virtual_server.py
+++ tests/suite/test_healthcheck_virtual_server.py
@@ -1,22 +1,13 @@
 import pytest
 import requests
-from settings import DEPLOYMENTS, TEST_DATA
-from suite.utils.custom_assertions import wait_and_assert_status_code
-from suite.utils.custom_resources_utils import create_crd_from_yaml, delete_crd
+from settings import TEST_DATA
 from suite.utils.resources_utils import (
-    create_service_from_yaml,
-    delete_service,
-    patch_rbac,
-    read_service,
-    replace_service,
+    create_secret_from_yaml,
+    delete_secret,
+    ensure_response_from_backend,
     wait_before_test,
 )
-from suite.utils.vs_vsr_resources_utils import (
-    create_virtual_server_from_yaml,
-    delete_virtual_server,
-    patch_virtual_server_from_yaml,
-)
-from suite.utils.yaml_utils import get_first_host_from_yaml, get_name_from_yaml, get_paths_from_vs_yaml
+from suite.utils.yaml_utils import get_first_host_from_yaml
 
 
 @pytest.mark.vs
@@ -31,18 +22,65 @@ from suite.utils.yaml_utils import get_first_host_from_yaml, get_name_from_yaml,
     ],
     indirect=True,
 )
-class TestHealthCheckVs:
-    def test_responses_after_setup(
+class TestHealthCheckVsHttp:
+    def test_responses_svc_insight_http(
         self, request, kube_apis, crd_ingress_controller, virtual_server_setup, ingress_controller_endpoint
     ):
-        print("\nStep 1: initial check")
-        wait_before_test()
-        wait_and_assert_status_code(200, virtual_server_setup.backend_1_url, virtual_server_setup.vs_host)
-        wait_and_assert_status_code(200, virtual_server_setup.backend_2_url, virtual_server_setup.vs_host)
-
+        """test responses from service insight endpoint with http"""
         vs_source = f"{TEST_DATA}/virtual-server/standard/virtual-server.yaml"
         host = get_first_host_from_yaml(vs_source)
         req_url = f"http://{ingress_controller_endpoint.public_ip}:{ingress_controller_endpoint.service_insight_port}/probe/{host}"
+        ensure_response_from_backend(req_url, virtual_server_setup.vs_host)
         resp = requests.get(req_url)
+        assert resp.status_code == 200, f"Expected 200 code for /probe/{host} but got {resp.status_code}"
+        print(resp.json())
+
+
+@pytest.fixture(scope="class")
+def https_secret_setup(request, kube_apis, test_namespace):
+    print("------------------------- Deploy Secret -----------------------------------")
+    secret_name = create_secret_from_yaml(kube_apis.v1, "nginx-ingress", f"{TEST_DATA}/service-insight/secret.yaml")
+
+    def fin():
+        delete_secret(kube_apis.v1, secret_name, "nginx-ingress")
+
+    request.addfinalizer(fin)
+
+
+@pytest.mark.vs
+@pytest.mark.skip_for_nginx_oss
+@pytest.mark.parametrize(
+    "crd_ingress_controller, virtual_server_setup",
+    [
+        (
+            {
+                "type": "complete",
+                "extra_args": [
+                    f"-enable-custom-resources",
+                    f"-enable-service-insight",
+                    f"-service-insight-tls-secret=nginx-ingress/test-secret",
+                ],
+            },
+            {"example": "virtual-server", "app_type": "simple"},
+        )
+    ],
+    indirect=True,
+)
+class TestHealthCheckVsHttps:
+    def test_responses_svc_insight_https(
+        self,
+        request,
+        kube_apis,
+        https_secret_setup,
+        ingress_controller_endpoint,
+        crd_ingress_controller,
+        virtual_server_setup,
+    ):
+        """test responses from service insight endpoint with https"""
+        vs_source = f"{TEST_DATA}/virtual-server/standard/virtual-server.yaml"
+        host = get_first_host_from_yaml(vs_source)
+        req_url = f"https://{ingress_controller_endpoint.public_ip}:{ingress_controller_endpoint.service_insight_port}/probe/{host}"
+        ensure_response_from_backend(req_url, virtual_server_setup.vs_host)
+        resp = requests.get(req_url, verify=False)
         assert resp.status_code == 200, f"Expected 200 code for /probe/{host} but got {resp.status_code}"
         print(resp.json())
