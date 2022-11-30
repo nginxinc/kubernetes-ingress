ab945e890982e362825a864fec75b8c0c230664b
Venktesh <ve.patel@f5.com>
2022-11-30 13:23:56 +0000

    add test for replica change

--- tests/suite/test_virtual_server_service_insight.py
+++ tests/suite/test_virtual_server_service_insight.py
@@ -5,6 +5,7 @@ from suite.utils.resources_utils import (
     create_secret_from_yaml,
     delete_secret,
     ensure_response_from_backend,
+    patch_deployment_from_yaml,
     wait_before_test,
 )
 from suite.utils.yaml_utils import get_first_host_from_yaml
@@ -33,8 +34,7 @@ class TestHealthCheckVsHttp:
         ensure_response_from_backend(req_url, virtual_server_setup.vs_host)
         resp = requests.get(req_url)
         assert resp.status_code == 200, f"Expected 200 code for /probe/{host} but got {resp.status_code}"
-        print(resp.json())
-
+        assert resp.json() == {'Total': 3, 'Up': 3, 'Unhealthy': 0}
 
 @pytest.fixture(scope="class")
 def https_secret_setup(request, kube_apis, test_namespace):
@@ -83,4 +83,27 @@ class TestHealthCheckVsHttps:
         ensure_response_from_backend(req_url, virtual_server_setup.vs_host)
         resp = requests.get(req_url, verify=False)
         assert resp.status_code == 200, f"Expected 200 code for /probe/{host} but got {resp.status_code}"
-        print(resp.json())
+        assert resp.json() == {'Total': 3, 'Up': 3, 'Unhealthy': 0} 
+
+    def test_responses_svc_insight_update_pods(
+        self,
+        request,
+        kube_apis,
+        https_secret_setup,
+        ingress_controller_endpoint,
+        test_namespace,
+        crd_ingress_controller,
+        virtual_server_setup,
+    ):
+        vs_source = f"{TEST_DATA}/virtual-server/standard/virtual-server.yaml"
+        host = get_first_host_from_yaml(vs_source)
+        req_url = f"https://{ingress_controller_endpoint.public_ip}:{ingress_controller_endpoint.service_insight_port}/probe/{host}"
+        ensure_response_from_backend(req_url, virtual_server_setup.vs_host)
+
+        # patch backend1 deployment with 5 replicas
+        patch_deployment_from_yaml(kube_apis.apps_v1_api, test_namespace, f"{TEST_DATA}/service-insight/app.yaml")
+        ensure_response_from_backend(req_url, virtual_server_setup.vs_host)
+        wait_before_test()
+        resp = requests.get(req_url, verify=False)
+        assert resp.status_code == 200, f"Expected 200 code for /probe/{host} but got {resp.status_code}"
+        assert resp.json() == {'Total': 6, 'Up': 6, 'Unhealthy': 0}
