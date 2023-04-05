decab8099d43fac06da7711f2b43b11febdf04ff
shaun-nx <s.odonovan@f5.com>
2023-04-05 11:29:41 +0100

    Add test with multiple VirtualServers

--- tests/suite/test_jwt_policies_jwksuri.py
+++ tests/suite/test_jwt_policies_jwksuri.py
@@ -6,7 +6,7 @@ import requests
 from settings import TEST_DATA
 from suite.utils.policy_resources_utils import create_policy_from_yaml, delete_policy
 from suite.utils.resources_utils import replace_configmap_from_yaml, wait_before_test
-from suite.utils.vs_vsr_resources_utils import delete_and_create_vs_from_yaml, patch_v_s_route_from_yaml
+from suite.utils.vs_vsr_resources_utils import delete_and_create_vs_from_yaml, patch_v_s_route_from_yaml, create_virtual_server_from_yaml, delete_virtual_server
 
 std_vs_src = f"{TEST_DATA}/virtual-server/standard/virtual-server.yaml"
 jwt_pol_valid_src = f"{TEST_DATA}/jwt-policy-jwksuri/policies/jwt-policy-valid.yaml"
@@ -14,6 +14,7 @@ jwt_pol_invalid_src = f"{TEST_DATA}/jwt-policy-jwksuri/policies/jwt-policy-inval
 jwt_vs_spec_src = f"{TEST_DATA}/jwt-policy-jwksuri/virtual-server/virtual-server-policy-spec.yaml"
 jwt_vs_route_src = f"{TEST_DATA}/jwt-policy-jwksuri/virtual-server/virtual-server-policy-route.yaml"
 jwt_vs_route_subpath_src = f"{TEST_DATA}/jwt-policy-jwksuri/virtual-server/virtual-server-policy-route-subpath.yaml"
+jwt_vs_route_subpath_diff_host_src = f"{TEST_DATA}/jwt-policy-jwksuri/virtual-server/virtual-server-policy-route-subpath-diff-host.yaml"
 jwt_vs_invalid_pol_spec_src = f"{TEST_DATA}/jwt-policy-jwksuri/virtual-server/virtual-server-invalid-policy-spec.yaml"
 jwt_vs_invalid_pol_route_src = f"{TEST_DATA}/jwt-policy-jwksuri/virtual-server/virtual-server-invalid-policy-route.yaml"
 jwt_vs_invalid_pol_route_subpath_src = f"{TEST_DATA}/jwt-policy-jwksuri/virtual-server/virtual-server-invalid-policy-route-subpath.yaml"
@@ -46,7 +47,7 @@ def get_token(request):
 
 @pytest.mark.skip_for_nginx_oss
 @pytest.mark.policies
-@pytest.mark.skip(reason="issues with ingressClass")
+# @pytest.mark.skip(reason="issues with ingressClass")
 @pytest.mark.parametrize(
     "crd_ingress_controller, virtual_server_setup",
     [
@@ -262,3 +263,116 @@ class TestJWTPoliciesVsJwksuri:
         assert resp_no_token.status_code == 401 and f"Authorization Required" in resp_no_token.text
         assert resp_valid_token.status_code == 200 and f"Request ID:" in resp_valid_token.text
         assert resp_pol_deleted.status_code == 500 and f"Internal Server Error" in resp_pol_deleted.text
+
+    def test_jwt_policy_subroute_jwksuri_multiple_vs(
+        self,
+        request,
+        kube_apis,
+        ingress_controller_prerequisites,
+        crd_ingress_controller,
+        virtual_server_setup,
+        test_namespace,
+    ):
+        """
+        Test jwt-policy applied to two Virtual Servers with different hosts and the same subpaths
+        """
+        replace_configmap_from_yaml(
+            kube_apis.v1,
+            ingress_controller_prerequisites.config_map["metadata"]["name"],
+            ingress_controller_prerequisites.namespace,
+            jwt_cm_src,
+        )
+        pol_name = create_policy_from_yaml(kube_apis.custom_objects, jwt_pol_valid_src, test_namespace)
+        wait_before_test()
+
+        print(f"Patch first vs with policy: {jwt_vs_route_subpath_src}")
+        delete_and_create_vs_from_yaml(
+            kube_apis.custom_objects,
+            virtual_server_setup.vs_name,
+            jwt_vs_route_subpath_src,
+            virtual_server_setup.namespace,
+        )
+
+        print(f"Create second vs with policy: {jwt_vs_route_subpath_diff_host_src}")
+        create_virtual_server_from_yaml(
+            kube_apis.custom_objects,
+            jwt_vs_route_subpath_diff_host_src,
+            virtual_server_setup.namespace,
+        )
+
+        wait_before_test()
+
+        resp_1_no_token = mock.Mock()
+        resp_1_no_token.status_code == 502
+
+        resp_2_no_token = mock.Mock()
+        resp_2_no_token.status_code == 502
+        counter = 0
+
+        while resp_1_no_token.status_code != 401 and counter < 3:
+            resp_1_no_token = requests.get(
+                virtual_server_setup.backend_1_url + "/subpath1",
+                headers={"host": virtual_server_setup.vs_host},
+            )
+            wait_before_test()
+            counter += 1
+
+        counter = 0
+
+        while resp_2_no_token.status_code != 401 and counter < 3:
+            resp_2_no_token = requests.get(
+                virtual_server_setup.backend_1_url + "/subpath1",
+                headers={"host": "virtual-server-2.example.com"},
+            )
+            wait_before_test()
+            counter += 1
+
+        token = get_token(request)
+
+        resp_1_valid_token = requests.get(
+            virtual_server_setup.backend_1_url + "/subpath1",
+            headers={"host": virtual_server_setup.vs_host, "token": token},
+            timeout=5,
+        )
+
+        resp_2_valid_token = requests.get(
+            virtual_server_setup.backend_1_url + "/subpath1",
+            headers={"host": "virtual-server-2.example.com", "token": token},
+            timeout=5,
+        )
+
+        delete_policy(kube_apis.custom_objects, pol_name, test_namespace)
+        wait_before_test()
+
+        resp_1_pol_deleted = requests.get(
+            virtual_server_setup.backend_1_url + "/subpath1",
+            headers={"host": virtual_server_setup.vs_host, "token": token},
+            timeout=5,
+        )
+
+        resp_2_pol_deleted = requests.get(
+            virtual_server_setup.backend_1_url + "/subpath1",
+            headers={"host": "virtual-server-2.example.com", "token": token},
+            timeout=5,
+        )
+
+        delete_and_create_vs_from_yaml(
+            kube_apis.custom_objects,
+            virtual_server_setup.vs_name,
+            std_vs_src,
+            virtual_server_setup.namespace,
+        )
+
+        delete_virtual_server(
+            kube_apis.custom_objects,
+            "virtual-server-2",
+            virtual_server_setup.namespace,
+        )
+
+        assert resp_1_no_token.status_code == 401 and f"Authorization Required" in resp_1_no_token.text
+        assert resp_1_valid_token.status_code == 200 and f"Request ID:" in resp_1_valid_token.text
+        assert resp_1_pol_deleted.status_code == 500 and f"Internal Server Error" in resp_1_pol_deleted.text
+
+        assert resp_2_no_token.status_code == 401 and f"Authorization Required" in resp_2_no_token.text
+        assert resp_2_valid_token.status_code == 200 and f"Request ID:" in resp_2_valid_token.text
+        assert resp_2_pol_deleted.status_code == 500 and f"Internal Server Error" in resp_2_pol_deleted.text
