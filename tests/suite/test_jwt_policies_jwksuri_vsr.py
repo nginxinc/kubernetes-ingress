58a400b061a4a43de6300d5f148cdd93dac0f067
shaun-nx <s.odonovan@f5.com>
2023-04-05 13:52:47 +0100

    Remove fixture

--- tests/suite/test_jwt_policies_jwksuri_vsr.py
+++ tests/suite/test_jwt_policies_jwksuri_vsr.py
@@ -44,7 +44,6 @@ def get_token(request):
 
 @pytest.mark.skip_for_nginx_oss
 @pytest.mark.policies
-@pytest.mark.skip(reason="issues with ingressClass")
 @pytest.mark.parametrize(
     "crd_ingress_controller, v_s_route_setup",
     [
