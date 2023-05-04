e83a96f333f62ad6cb24f3dd75a4c8b83de96f18
Venktesh <ve.patel@f5.com>
2023-05-04 12:43:13 +0100

    typo

--- tests/suite/test_ingress_mtls.py
+++ tests/suite/test_ingress_mtls.py
@@ -215,6 +215,7 @@ class TestIngressMtlsPolicyVS:
             virtual_server_setup.namespace,
         )
         wait_before_test()
+        ssl_exception=""
         resp = mock.Mock()
         resp.status_code == 502
         counter = 0
@@ -311,7 +312,6 @@ class TestIngressMtlsPolicyVS:
             virtual_server_setup.namespace,
         )
         wait_before_test()
-        ssl_exception = ""
         resp = mock.Mock()
         resp.status_code == 502
         counter = 0
