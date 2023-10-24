a242d7843904a6165149e81b5a631877907b9d93
Ian Yong <ianyongyc@gmail.com>
2023-10-24 23:12:17 +0800

    Add tests for Go template string comparison functions.
    These string comparison functions come from Go's standard library,
    so there isn't really a need to test them extensively. Instead,
    these test cases are mainly to assert that these functions can be
    correctly used within the template executor.

---
+++ internal/configs/version2/template_helper_test.go
@@ -0,0 +1,196 @@
+package version2
+
+import (
+	"bytes"
+	"testing"
+	"text/template"
+)
+
+func TestContainsSubstring(t *testing.T) {
+	type testCase struct {
+		InputString string
+		Substring   string
+		expected    string
+	}
+	t.Parallel()
+
+	tmpl := newContainsTemplate(t)
+	testCases := []testCase{
+		{InputString: "foo", Substring: "foo", expected: "true"},
+		{InputString: "foobar", Substring: "foo", expected: "true"},
+		{InputString: "foo", Substring: "", expected: "true"},
+		{InputString: "foo", Substring: "bar", expected: "false"},
+		{InputString: "foo", Substring: "foobar", expected: "false"},
+		{InputString: "", Substring: "foo", expected: "false"},
+	}
+
+	for _, tc := range testCases {
+		var buf bytes.Buffer
+		err := tmpl.Execute(&buf, tc)
+		if err != nil {
+			t.Fatalf("Failed to execute the template %v", err)
+		}
+		if buf.String() != tc.expected {
+			t.Errorf("Template generated wrong config, got %v but expected %v.", buf.String(), tc.expected)
+		}
+	}
+}
+
+func TestHasPrefix(t *testing.T) {
+	type testCase struct {
+		InputString string
+		Prefix      string
+		expected    string
+	}
+	t.Parallel()
+
+	tmpl := newHasPrefixTemplate(t)
+	testCases := []testCase{
+		{InputString: "foo", Prefix: "foo", expected: "true"},
+		{InputString: "foo", Prefix: "f", expected: "true"},
+		{InputString: "foo", Prefix: "", expected: "true"},
+		{InputString: "foo", Prefix: "oo", expected: "false"},
+		{InputString: "foo", Prefix: "bar", expected: "false"},
+		{InputString: "foo", Prefix: "foobar", expected: "false"},
+	}
+
+	for _, tc := range testCases {
+		var buf bytes.Buffer
+		err := tmpl.Execute(&buf, tc)
+		if err != nil {
+			t.Fatalf("Failed to execute the template %v", err)
+		}
+		if buf.String() != tc.expected {
+			t.Errorf("Template generated wrong config, got %v but expected %v.", buf.String(), tc.expected)
+		}
+	}
+}
+
+func TestHasSuffix(t *testing.T) {
+	type testCase struct {
+		InputString string
+		Suffix      string
+		expected    string
+	}
+	t.Parallel()
+
+	tmpl := newHasSuffixTemplate(t)
+	testCases := []testCase{
+		{InputString: "bar", Suffix: "bar", expected: "true"},
+		{InputString: "bar", Suffix: "r", expected: "true"},
+		{InputString: "bar", Suffix: "", expected: "true"},
+		{InputString: "bar", Suffix: "ba", expected: "false"},
+		{InputString: "bar", Suffix: "foo", expected: "false"},
+		{InputString: "bar", Suffix: "foobar", expected: "false"},
+	}
+
+	for _, tc := range testCases {
+		var buf bytes.Buffer
+		err := tmpl.Execute(&buf, tc)
+		if err != nil {
+			t.Fatalf("Failed to execute the template %v", err)
+		}
+		if buf.String() != tc.expected {
+			t.Errorf("Template generated wrong config, got %v but expected %v.", buf.String(), tc.expected)
+		}
+	}
+}
+
+func TestToLowerInputString(t *testing.T) {
+	type testCase struct {
+		InputString string
+		expected    string
+	}
+	t.Parallel()
+
+	tmpl := newToLowerTemplate(t)
+	testCases := []testCase{
+		{InputString: "foobar", expected: "foobar"},
+		{InputString: "FOOBAR", expected: "foobar"},
+		{InputString: "fOoBaR", expected: "foobar"},
+		{InputString: "", expected: ""},
+	}
+
+	for _, tc := range testCases {
+		var buf bytes.Buffer
+		err := tmpl.Execute(&buf, tc)
+		if err != nil {
+			t.Fatalf("Failed to execute the template %v", err)
+		}
+		if buf.String() != tc.expected {
+			t.Errorf("Template generated wrong config, got %v but expected %v.", buf.String(), tc.expected)
+		}
+	}
+}
+
+func TestToUpperInputString(t *testing.T) {
+	type testCase struct {
+		InputString string
+		expected    string
+	}
+	t.Parallel()
+
+	tmpl := newToUpperTemplate(t)
+	testCases := []testCase{
+		{InputString: "foobar", expected: "FOOBAR"},
+		{InputString: "FOOBAR", expected: "FOOBAR"},
+		{InputString: "fOoBaR", expected: "FOOBAR"},
+		{InputString: "", expected: ""},
+	}
+
+	for _, tc := range testCases {
+		var buf bytes.Buffer
+		err := tmpl.Execute(&buf, tc)
+		if err != nil {
+			t.Fatalf("Failed to execute the template %v", err)
+		}
+		if buf.String() != tc.expected {
+			t.Errorf("Template generated wrong config, got %v but expected %v.", buf.String(), tc.expected)
+		}
+	}
+}
+
+func newContainsTemplate(t *testing.T) *template.Template {
+	t.Helper()
+	tmpl, err := template.New("testTemplate").Funcs(helperFunctions).Parse(`{{contains .InputString .Substring}}`)
+	if err != nil {
+		t.Fatalf("Failed to parse template: %v", err)
+	}
+	return tmpl
+}
+
+func newHasPrefixTemplate(t *testing.T) *template.Template {
+	t.Helper()
+	tmpl, err := template.New("testTemplate").Funcs(helperFunctions).Parse(`{{hasPrefix .InputString .Prefix}}`)
+	if err != nil {
+		t.Fatalf("Failed to parse template: %v", err)
+	}
+	return tmpl
+}
+
+func newHasSuffixTemplate(t *testing.T) *template.Template {
+	t.Helper()
+	tmpl, err := template.New("testTemplate").Funcs(helperFunctions).Parse(`{{hasSuffix .InputString .Suffix}}`)
+	if err != nil {
+		t.Fatalf("Failed to parse template: %v", err)
+	}
+	return tmpl
+}
+
+func newToLowerTemplate(t *testing.T) *template.Template {
+	t.Helper()
+	tmpl, err := template.New("testTemplate").Funcs(helperFunctions).Parse(`{{toLower .InputString}}`)
+	if err != nil {
+		t.Fatalf("Failed to parse template: %v", err)
+	}
+	return tmpl
+}
+
+func newToUpperTemplate(t *testing.T) *template.Template {
+	t.Helper()
+	tmpl, err := template.New("testTemplate").Funcs(helperFunctions).Parse(`{{toUpper .InputString}}`)
+	if err != nil {
+		t.Fatalf("Failed to parse template: %v", err)
+	}
+	return tmpl
+}
