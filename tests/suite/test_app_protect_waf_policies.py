b8ff22e5772f344fa0fe4b3c0e1d525795dd9825
Paul Abel <p.abel@f5.com>
2023-12-12 12:32:02 +0000

    split appProtect tests to speed up pipeline

--- tests/suite/test_app_protect_waf_policies.py
+++ tests/suite/test_app_protect_waf_policies.py
@@ -110,6 +110,7 @@ def assert_valid_responses(response) -> None:
 
 @pytest.mark.skip_for_nginx_oss
 @pytest.mark.appprotect
+@pytest.mark.appprotect_waf_policies
 @pytest.mark.parametrize(
     "crd_ingress_controller_with_ap, virtual_server_setup",
     [
@@ -366,6 +367,7 @@ class TestAppProtectWAFPolicyVS:
 
 @pytest.mark.skip_for_nginx_oss
 @pytest.mark.appprotect
+@pytest.mark.appprotect_waf_policies
 @pytest.mark.parametrize(
     "crd_ingress_controller_with_ap, v_s_route_setup",
     [
