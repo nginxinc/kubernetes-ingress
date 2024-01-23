693415d21639d6da9ac58a2bb123ff8a5ee561a6
Paul Abel <p.abel@f5.com>
2024-01-23 12:36:56 +0000

    split release notes into new script

--- .github/scripts/release-version-update.sh
+++ .github/scripts/release-version-update.sh
@@ -117,17 +117,3 @@ for i in ${docs_files}; do
         exit 2
     fi
 done
-
-# update releases docs
-file_path=${DOCS_TO_UPDATE_FOLDER}/releases.md
-if [ "${DEBUG}" != "false" ]; then
-    echo "Processing ${file_path}"
-fi
-file_name=$(basename "${file_path}")
-mv "${file_path}" "${TMPDIR}/${file_name}"
-cat "${TMPDIR}/${file_name}" | sed -e "8r ${ROOTDIR}/hack/changelog-template.txt" | sed -e "s/%%TITLE%%/## $ic_version/g" -e "s/%%IC_VERSION%%/$ic_version/g" -e "s/%%HELM_CHART_VERSION%%/$helm_chart_version/g" > ${file_path}
-if [ $? -ne 0 ]; then
-    echo "ERROR: failed processing ${file_path}"
-    mv "${TMPDIR}/${file_name}" "${file_path}"
-    exit 2
-fi
