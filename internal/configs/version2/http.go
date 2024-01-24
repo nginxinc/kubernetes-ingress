270553eeb4d6489e8db22b89262c779426e2ee6f
llomgui <llomgui@live.fr>
2024-01-24 23:36:05 +0100

    Evolve oidc logout

--- internal/configs/version2/http.go
+++ internal/configs/version2/http.go
@@ -122,16 +122,18 @@ type EgressMTLS struct {
 
 // OIDC holds OIDC configuration data.
 type OIDC struct {
-	AuthEndpoint      string
-	ClientID          string
-	ClientSecret      string
-	JwksURI           string
-	Scope             string
-	TokenEndpoint     string
-	RedirectURI       string
-	ZoneSyncLeeway    int
-	AuthExtraArgs     string
-	AccessTokenEnable bool
+	AuthEndpoint       string
+	LogoutEndpoint     string
+	ClientID           string
+	ClientSecret       string
+	JwksURI            string
+	Scope              string
+	TokenEndpoint      string
+	RedirectURI        string
+	RedirectPostLogout string
+	ZoneSyncLeeway     int
+	AuthExtraArgs      string
+	AccessTokenEnable  bool
 }
 
 // WAF defines WAF configuration.
