a60f7e27b2d19731c31555b60f02ab942cc33400
Madhu RAJAGOPAL <m.rajagopal@f5.com>
2024-02-28 17:07:42 +1300

    Issue 4837: chore: main.go refactor

--- cmd/nginx-ingress/main_test.go
+++ cmd/nginx-ingress/main_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/nginxinc/kubernetes-ingress/internal/metrics/collectors"
+)
+
+func TestGetNginxVersionInfo(t *testing.T) {
+	os.Args = append(os.Args, "-nginx-plus")
+	os.Args = append(os.Args, "-proxy")
+	os.Args = append(os.Args, "test-proxy")
+	flag.Parse()
+	constLabels := map[string]string{"class": *ingressClass}
+	mc := collectors.NewLocalManagerMetricsCollector(constLabels)
+	nginxManager, _ := createNginxManager(mc)
+	nginxInfo, _ := getNginxVersionInfo(nginxManager)
+	if nginxInfo.String() == "" {
+		t.Errorf("Error when getting nginx version, empty string")
+	}
+}
