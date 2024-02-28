05736e301e11bdca88875f811030ab904ee90631
Madhu RAJAGOPAL <m.rajagopal@f5.com>
2024-02-28 17:11:32 +1300

    Issue 4837: chore: main.go refactor * Added initial unit-tests for some functions

--- cmd/nginx-ingress/main_test.go
+++ cmd/nginx-ingress/main_test.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"testing"
 
-	"github.com/nginxinc/kubernetes-ingress/internal/metrics/collectors"
 )
 
 func TestGetNginxVersionInfo(t *testing.T) {
@@ -19,5 +19,82 @@ func TestGetNginxVersionInfo(t *testing.T) {
 	nginxInfo, _ := getNginxVersionInfo(nginxManager)
 	if nginxInfo.String() == "" {
 		t.Errorf("Error when getting nginx version, empty string")
+	}
+}
+
+func TestGetAppProtectVersionInfo(t *testing.T) {
+	dataToWrite := "1.2.3\n"
+	f, err := os.CreateTemp("/var/tmp/", "dat1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString(dataToWrite)
+	if err != nil {
+		fmt.Println(err)
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	if err := f.Close(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	version, err := getAppProtectVersionInfo()
+	if err != nil {
+		t.Errorf("Error reading AppProtect Version file #{err}\n")
+	}
+
+	if version == "" {
+		t.Errorf("Error with AppProtect Version, is empty")
+	}
+}
+
+func TestCreateGlobalConfigurationValidator(t *testing.T) {
+	globalConfiguration := conf_v1.GlobalConfiguration{
+		Spec: conf_v1.GlobalConfigurationSpec{
+			Listeners: []conf_v1.Listener{
+				{
+					Name:     "tcp-listener",
+					Port:     53,
+					Protocol: "TCP",
+				},
+				{
+					Name:     "udp-listener",
+					Port:     53,
+					Protocol: "UDP",
+				},
+			},
+		},
+	}
+
+	gcv := createGlobalConfigurationValidator()
+
+	if err := gcv.ValidateGlobalConfiguration(&globalConfiguration); err != nil {
+		t.Errorf("ValidateGlobalConfiguration() returned error %v for valid input", err)
+	}
+
+	incorrectGlobalConf := conf_v1.GlobalConfiguration{
+		Spec: conf_v1.GlobalConfigurationSpec{
+			Listeners: []conf_v1.Listener{
+				{
+					Name:     "tcp-listener",
+					Port:     53,
+					Protocol: "TCPT",
+				},
+				{
+					Name:     "udp-listener",
+					Port:     53,
+					Protocol: "UDP",
+				},
+			},
+		},
+	}
+
+	if err := gcv.ValidateGlobalConfiguration(&incorrectGlobalConf); err == nil {
+		t.Errorf("ValidateGlobalConfiguration() returned error %v for invalid input", err)
 	}
 }
