86e75220db955162f95065f3d8e78e9510b82fee
Madhu RAJAGOPAL <m.rajagopal@f5.com>
2024-03-05 13:27:32 +1300

    Issue 4837: chore: main_test.go unit-tests * Add unit-test for confirmMinimumK8sVersionCriteria()

--- cmd/nginx-ingress/main_test.go
+++ cmd/nginx-ingress/main_test.go
@@ -8,13 +8,15 @@ import (
 	"testing"
 
 	"github.com/nginxinc/kubernetes-ingress/internal/k8s"
-	networkingv1 "k8s.io/api/networking/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
 
 	"github.com/nginxinc/kubernetes-ingress/internal/metrics/collectors"
 	conf_v1 "github.com/nginxinc/kubernetes-ingress/pkg/apis/configuration/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiVersion "k8s.io/apimachinery/pkg/version"
+	fakeDisc "k8s.io/client-go/discovery/fake"
 )
 
 // Test for getNginxVersionInfo()
@@ -38,7 +40,7 @@ func TestGetAppProtectVersionInfo(t *testing.T) {
 	versionFile := path.Base(appProtectVersionPath)
 	f, err := os.CreateTemp(dirPath, versionFile)
 	if err != nil {
-		fmt.Println(err)
+		t.Errorf("Error creating temp directory: %v", err)
 		return
 	}
 	l, err := f.WriteString(dataToWrite)
@@ -57,7 +59,7 @@ func TestGetAppProtectVersionInfo(t *testing.T) {
 	}
 	version, err := getAppProtectVersionInfo()
 	if err != nil {
-		t.Errorf("Error reading AppProtect Version file #{err}\n")
+		t.Errorf("Error reading AppProtect Version file: %v", err)
 	}
 
 	if version == "" {
@@ -160,6 +162,58 @@ func TestValidateIngressClass(t *testing.T) {
 
 		if err := validateIngressClass(inValidData[0].clientset); err == nil {
 			t.Fatalf("validateIngressClass() returned no error for invalid input, error: %v", err)
+		}
+	}
+}
+
+func TestMinimumK8sVersion3(t *testing.T) {
+	// Create a fake client.
+	clientset := fake.NewSimpleClientset()
+
+	// Override the ServerVersion method on the fake Discovery client
+	discoveryClient, ok := clientset.Discovery().(*fakeDisc.FakeDiscovery)
+	if !ok {
+		fmt.Println("couldn't convert Discovery() to *FakeDiscovery")
+	}
+
+	// This test block is when the correct/expected k8s version is returned
+	{
+		correctVersion := &apiVersion.Info{
+			Major: "1", Minor: "22", GitVersion: "v1.22.2",
+		}
+		discoveryClient.FakedServerVersion = correctVersion
+
+		// Get the server version as a sanity check
+		actualVersion, err := discoveryClient.ServerVersion()
+		if err != nil {
+			t.Fatalf("Failed to get server version: %v", err)
+		}
+		fmt.Printf("Kubernetes version: %s\n", actualVersion.String())
+
+		// Verify if the mocked server version is as expected.
+		if err := confirmMinimumK8sVersionCriteria(clientset); err != nil {
+			t.Fatalf("Error in checking minimum k8s version: %v", err)
+		}
+	}
+
+	// This test block is when the incorrect/unexpected k8s version is returned
+	// i.e. not the min supported version
+	{
+		wrongVersion := &apiVersion.Info{
+			Major: "1", Minor: "19", GitVersion: "v1.19.2",
+		}
+		discoveryClient.FakedServerVersion = wrongVersion
+
+		// Get the server version as a sanity check
+		actualVersion, err := discoveryClient.ServerVersion()
+		if err != nil {
+			t.Fatalf("Failed to get server version: %v", err)
+		}
+		fmt.Printf("Kubernetes version: %s\n", actualVersion.String())
+
+		// Verify if the mocked server version returns an error as we are testing for < 1.22 (v1.19.2).
+		if err := confirmMinimumK8sVersionCriteria(clientset); err == nil {
+			t.Fatalf("Expected an error when checking minimum k8s version but got none: %v", err)
 		}
 	}
 }
