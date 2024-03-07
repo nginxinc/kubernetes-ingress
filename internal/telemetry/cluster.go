a26917e997e718b9f7efb249a4f841f92e3d4e9b
Jakub Jarosz <j.jarosz@f5.com>
2024-03-07 18:51:31 +0000

    Add Platform name to telemetry data

--- internal/telemetry/cluster.go
+++ internal/telemetry/cluster.go
@@ -2,6 +2,8 @@ package telemetry
 
 import (
 	"context"
+	"errors"
+	"strings"
 
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -34,4 +36,43 @@ func (c *Collector) K8sVersion() (string, error) {
 		return "", err
 	}
 	return sv.String(), nil
+}
+
+// Platform returns a string representing a platform name
+// where the K8s cluster and NIC runs.
+func (c *Collector) Platform(ctx context.Context) (string, error) {
+	nodes, err := c.Config.K8sClientReader.CoreV1().Nodes().List(ctx, metaV1.ListOptions{})
+	if err != nil {
+		return "", err
+	}
+	if len(nodes.Items) == 0 {
+		return "", errors.New("no nodes in the cluster, cannot determine platform name")
+	}
+	return lookupPlatform(nodes.Items[0].Spec.ProviderID), nil
+}
+
+// lookupPlatform takes a string representing a K8s PlatformID
+// retrieved from a cluster node and returns a string
+// representing the platform name.
+func lookupPlatform(platformID string) string {
+	platform := strings.ToLower(platformID)
+	if strings.HasPrefix(platform, "aws") {
+		return "aws"
+	}
+	if strings.HasPrefix(platform, "azure") {
+		return "azure"
+	}
+	if strings.HasPrefix(platform, "gce") {
+		return "gke"
+	}
+	if strings.HasPrefix(platform, "kind") {
+		return "kind"
+	}
+	if strings.HasPrefix(platform, "vsphere") {
+		return "vsphere"
+	}
+	if strings.HasPrefix(platform, "k3s") {
+		return "k3s"
+	}
+	return "unknown"
 }
--- internal/telemetry/cluster_test.go
+++ internal/telemetry/cluster_test.go
@@ -80,6 +80,96 @@ func TestK8sVersionRetrievesClusterVersion(t *testing.T) {
 	}
 }
 
+func TestAwsPlatformDeterminesOwnName(t *testing.T) {
+	t.Parallel()
+
+	c := newTestCollectorForClusterWithNodes(t, nodeAWS)
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "aws"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestAzurePlatformDeterminesOwnName(t *testing.T) {
+	t.Parallel()
+
+	c := newTestCollectorForClusterWithNodes(t, nodeAzure)
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "azure"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestGcpPlatformDeterminesOwnName(t *testing.T) {
+	t.Parallel()
+
+	c := newTestCollectorForClusterWithNodes(t, nodeGCP)
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "gke"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestKindPlatformDeterminesOwnName(t *testing.T) {
+	t.Parallel()
+
+	c := newTestCollectorForClusterWithNodes(t, nodeKind)
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "kind"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestK3sPlatformDeterminesOwnName(t *testing.T) {
+	t.Parallel()
+
+	c := newTestCollectorForClusterWithNodes(t, nodeK3S)
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "k3s"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestVSpherePlatformDeterminesOwnName(t *testing.T) {
+	t.Parallel()
+
+	c := newTestCollectorForClusterWithNodes(t, nodeVSphere)
+	got, err := c.Platform(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "vsphere"
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
 // newTestCollectorForClusterWithNodes returns a telemetry collector configured
 // to simulate collecting data on a cluser with provided nodes.
 func newTestCollectorForClusterWithNodes(t *testing.T, nodes ...runtime.Object) *telemetry.Collector {
@@ -130,6 +220,90 @@ var (
 			Namespace: "default",
 		},
 		Spec: apiCoreV1.NodeSpec{},
+	}
+
+	nodeKind = &apiCoreV1.Node{
+		TypeMeta: metaV1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: apiCoreV1.NodeSpec{
+			ProviderID: "kind://docker/local/local-control-plane",
+		},
+	}
+
+	nodeAWS = &apiCoreV1.Node{
+		TypeMeta: metaV1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: apiCoreV1.NodeSpec{
+			ProviderID: "aws:///eu-central-1a/i-088b4f07708408cc0",
+		},
+	}
+
+	nodeAzure = &apiCoreV1.Node{
+		TypeMeta: metaV1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: apiCoreV1.NodeSpec{
+			ProviderID: "azure:///subscriptions/ba96ef31-4a42-40f5-8740-03f7e3c439eb/resourceGroups/mc_hibrid-weu_be3rr5ovr8ulf_westeurope/providers/Microsoft.Compute/virtualMachines/aks-pool1-27255451-0",
+		},
+	}
+
+	nodeGCP = &apiCoreV1.Node{
+		TypeMeta: metaV1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: apiCoreV1.NodeSpec{
+			ProviderID: "gce://gcp-banzaidevgcp-nprd-38306/europe-north1-a/gke-vzf3z1vvleco9-pool1-7e48d363-8qz1",
+		},
+	}
+
+	nodeK3S = &apiCoreV1.Node{
+		TypeMeta: metaV1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: apiCoreV1.NodeSpec{
+			ProviderID: "k3s://ip-1.2.3.4",
+		},
+	}
+
+	nodeVSphere = &apiCoreV1.Node{
+		TypeMeta: metaV1.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      "node",
+			Namespace: "default",
+		},
+		Spec: apiCoreV1.NodeSpec{
+			ProviderID: "vsphere://4232e3c7-d83c-d72b-758c-71d07a3d9310",
+		},
 	}
 
 	kubeNS = &apiCoreV1.Namespace{
