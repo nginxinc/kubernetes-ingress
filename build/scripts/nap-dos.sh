86d3322615581931bcd0e7934a8f9833a9a57bd5
Eoin O'Shaughnessy <e.oshaughnessy@f5.com>
2024-05-27 16:40:38 +0100

    refactor dockerfile to reduce layer duplication

---
+++ build/scripts/nap-dos.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p /root/app_protect_dos /etc/nginx/dos/policies /etc/nginx/dos/logconfs /shared/cores /var/log/adm /var/run/adm
+chmod 777 /shared/cores /var/log/adm /var/run/adm /etc/app_protect_dos
