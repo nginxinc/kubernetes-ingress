30c019dc9ef9a9c7398e025963e754d3086226b4
Jim Ryan <j.ryan@f5.com>
2024-05-28 10:07:58 +0100

    add updated njs

--- internal/configs/njs/apikey_auth.js
+++ internal/configs/njs/apikey_auth.js
@@ -1,25 +1,48 @@
 const c = require("crypto");
 
-function hash(r) {
-  const header_query_value = r.variables.header_query_value;
-  const hashed_value = c
-    .createHash("sha256")
-    .update(header_query_value)
-    .digest("hex");
-  return hashed_value;
+function hash_route(r) {
+    const header_query_value = r.variables.header_query_value_route;
+    const hashed_value = c.createHash('sha256').update(header_query_value).digest('hex');
+    return hashed_value;
 }
 
-function validate(r) {
-  const client_name = r.variables["apikey_auth_local_map"];
-  const header_query_value = r.variables.header_query_value;
-
-  if (!header_query_value) {
-    r.return(401, "401");
-  } else if (!client_name) {
-    r.return(403, "403");
-  } else {
-    r.return(204, "204");
-  }
+function hash_spec(r) {
+    const header_query_value = r.variables.header_query_value_spec;
+    const hashed_value = c.createHash('sha256').update(header_query_value).digest('hex');
+    return hashed_value;
 }
 
-export default { validate, hash };
+
+
+function validate_route(r) {
+    const client_name = r.variables['apikey_auth_local_map_route'];
+    const header_query_value = r.variables.header_query_value_route;
+
+    if (!header_query_value) {
+        r.return(401, "401")
+    }
+    else if (!client_name) {
+        r.return(403, "403")
+    }
+    else {
+        r.return(204, "204");
+    }
+}
+
+function validate_spec(r) {
+    const client_name = r.variables['apikey_auth_local_map_spec'];
+    const header_query_value = r.variables.header_query_value_spec;
+
+    if (!header_query_value) {
+        r.return(401, "401")
+    }
+    else if (!client_name) {
+        r.return(403, "403")
+    }
+    else {
+        r.return(204, "204");
+    }
+
+}
+
+export default { validate_route, validate_spec, hash_route, hash_spec };
