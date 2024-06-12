0dea2e5cc5b35864bbbe6a04be86e8f3ab56aa78
Shaun <s.odonovan@f5.com>
2024-06-12 17:22:46 +0100

    Integrate NAP WAF v5 (#5698)

--- internal/configs/config_params.go
+++ internal/configs/config_params.go
@@ -146,7 +146,9 @@ type StaticConfigParams struct {
 	NginxServiceMesh               bool
 	EnableInternalRoutes           bool
 	MainAppProtectLoadModule       bool
+	MainAppProtectV5LoadModule     bool
 	MainAppProtectDosLoadModule    bool
+	MainAppProtectV5EnforcerAddr   string
 	InternalRouteServerName        string
 	EnableLatencyMetrics           bool
 	EnableOIDC                     bool
@@ -156,6 +158,7 @@ type StaticConfigParams struct {
 	StaticSSLPath                  string
 	DynamicWeightChangesReload     bool
 	NginxVersion                   nginx.Version
+	AppProtectBundlePath           string
 }
 
 // GlobalConfigParams holds global configuration parameters. For now, it only holds listeners.
