774a2c6f3af862856a8bc72cc64ab720794521ef
Jim Ryan <j.ryan@f5.com>
2024-06-26 10:02:59 +0100

    remove unsed imports

--- tests/suite/test_transport_server_backup_service.py
+++ tests/suite/test_transport_server_backup_service.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 import pytest
 from settings import DEPLOYMENTS, TEST_DATA
 from suite.fixtures.fixtures import PublicEndpoint
--- tests/suite/test_transport_server_service_insight.py
+++ tests/suite/test_transport_server_service_insight.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from unittest import mock
 
 import pytest
