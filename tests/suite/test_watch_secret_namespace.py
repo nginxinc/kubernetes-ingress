127b424673c0d117629d858b34dc78323a8f75e3
Jim Ryan <j.ryan@f5.com>
2024-06-28 13:56:14 +0100

    Split VSR matrix in 3 (#5883)
    * Split VSR matrix in 3
    
    * fix quotes
    
    * simplify marker

--- tests/suite/test_watch_secret_namespace.py
+++ tests/suite/test_watch_secret_namespace.py
@@ -9,6 +9,7 @@ from suite.utils.ssl_utils import create_sni_session
 
 
 @pytest.mark.vsr
+@pytest.mark.vsr_secrets
 @pytest.mark.parametrize(
     "crd_ingress_controller, v_s_route_setup",
     [
@@ -63,6 +64,7 @@ class TestVSRWatchSecretNamespacesValid:
 
 
 @pytest.mark.vsr
+@pytest.mark.vsr_secrets
 @pytest.mark.parametrize(
     "crd_ingress_controller, v_s_route_setup",
     [
