4e81c84e737e2ac6d555109d34504f562427b3b5
Alex Fenlon <a.fenlon@f5.com>
2024-09-05 11:34:34 +0100

    Update crds and user facing listener names

--- pkg/apis/configuration/v1/types.go
+++ pkg/apis/configuration/v1/types.go
@@ -411,8 +411,8 @@ type GlobalConfigurationSpec struct {
 type Listener struct {
 	Name     string `json:"name"`
 	Port     int    `json:"port"`
-	IPv4IP   string `json:"ipv4ip"`
-	IPv6IP   string `json:"ipv6ip"`
+	IPv4IP   string `json:"ipv4"`
+	IPv6IP   string `json:"ipv6"`
 	Protocol string `json:"protocol"`
 	Ssl      bool   `json:"ssl"`
 }
