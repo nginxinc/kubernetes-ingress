12635f3fe09500501c1e7ba575b97af03665e106
Paul Abel <p.abel@f5.com>
2024-09-25 11:12:20 +0100

    add table tests

--- internal/logger/glog/handler_test.go
+++ internal/logger/glog/handler_test.go
@@ -2,6 +2,7 @@ package glog
 
 import (
 	"bytes"
+	"context"
 	"log/slog"
 	"regexp"
 	"testing"
@@ -17,9 +18,75 @@ func TestGlogFormat(t *testing.T) {
 	if !re.MatchString(got) {
 		t.Errorf("\ngot:\n%q\nwant:\n%q", got, wantre)
 	}
+}
 
-	buf.Reset()
+func TestGlogLogLevels(t *testing.T) {
+	testCases := []struct {
+		name   string
+		level  slog.Level
+		wantre string
+	}{
+		{
+			name:   "Trace level log message",
+			level:  LevelTrace,
+			wantre: `^I\d{8}\s\d+:\d+:\d+.\d{6}\s+\d+\s\w+\.go:\d+\]\s.*\s$`,
+		},
+		{
+			name:   "Debug level log message",
+			level:  LevelDebug,
+			wantre: `^I\d{8}\s\d+:\d+:\d+.\d{6}\s+\d+\s\w+\.go:\d+\]\s.*\s$`,
+		},
+		{
+			name:   "Info level log message",
+			level:  LevelInfo,
+			wantre: `^I\d{8}\s\d+:\d+:\d+.\d{6}\s+\d+\s\w+\.go:\d+\]\s.*\s$`,
+		},
+		{
+			name:   "Warning level log message",
+			level:  LevelWarning,
+			wantre: `^W\d{8}\s\d+:\d+:\d+.\d{6}\s+\d+\s\w+\.go:\d+\]\s.*\s$`,
+		},
+		{
+			name:   "Error level log message",
+			level:  LevelError,
+			wantre: `^E\d{8}\s\d+:\d+:\d+.\d{6}\s+\d+\s\w+\.go:\d+\]\s.*\s$`,
+		},
+		{
+			name:   "Fatal level log message",
+			level:  LevelFatal,
+			wantre: `^F\d{8}\s\d+:\d+:\d+.\d{6}\s+\d+\s\w+\.go:\d+\]\s.*\s$`,
+		},
+	}
+	t.Parallel()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			l := slog.New(New(&buf, &Options{Level: tc.level}))
+			l.Log(context.Background(), tc.level, "test")
+			got := buf.String()
+			re := regexp.MustCompile(tc.wantre)
+			if !re.MatchString(got) {
+				t.Errorf("\ngot:\n%q\nwant:\n%q", got, tc.wantre)
+			}
+		})
+	}
+}
+
+func TestGlogDefaultLevel(t *testing.T) {
+	var buf bytes.Buffer
+	l := slog.New(New(&buf, nil))
+
 	l.Debug("test")
+	if got := buf.Len(); got != 0 {
+		t.Errorf("got buf.Len() = %d, want 0", got)
+	}
+}
+
+func TestGlogHigherLevel(t *testing.T) {
+	var buf bytes.Buffer
+	l := slog.New(New(&buf, &Options{Level: LevelError}))
+
+	l.Info("test")
 	if got := buf.Len(); got != 0 {
 		t.Errorf("got buf.Len() = %d, want 0", got)
 	}
