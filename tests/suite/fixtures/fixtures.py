03e935ade3d8e6d1d8fc366ec7c515b0003d05f6
Venktesh <ve.patel@f5.com>
2024-11-28 15:16:19 +0000

    update WAFv5 version

--- tests/suite/fixtures/fixtures.py
+++ tests/suite/fixtures/fixtures.py
@@ -335,6 +335,7 @@ def cli_arguments(request) -> {}:
     print(f"Tests will run against the IC of type: {result['ic-type']}")
     if result["ic-type"] == "nginx-plus-ingress":
         print(f"Tests will use the Plus JWT")
+        result["plus-jwt"] = request.config.getoption("--plus-jwt")
 
     result["replicas"] = request.config.getoption("--replicas")
     print(f"Number of pods spun up will be : {result['replicas']}")
