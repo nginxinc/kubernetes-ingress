13fa1382fa4245aa47090d899cfee5b604515ed1
Alex Fenlon <a.fenlon@f5.com>
2024-12-02 17:48:27 +0000

    add license secret and mgmt config map

--- internal/configs/configurator_test.go
+++ internal/configs/configurator_test.go
@@ -51,6 +51,7 @@ func createTestConfigurator(t *testing.T) *Configurator {
 		NginxManager:            manager,
 		StaticCfgParams:         createTestStaticConfigParams(),
 		Config:                  NewDefaultConfigParams(context.Background(), false),
+		MGMTCfgParams:           NewDefaultMGMTConfigParams(context.Background()),
 		TemplateExecutor:        templateExecutor,
 		TemplateExecutorV2:      templateExecutorV2,
 		LatencyCollector:        nil,
@@ -101,7 +102,7 @@ func TestConfiguratorUpdatesConfigWithNilCustomMainTemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		MainTemplate: nil,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -119,7 +120,7 @@ func TestConfiguratorUpdatesConfigWithCustomMainTemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		MainTemplate: &customTestMainTemplate,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -141,7 +142,7 @@ func TestConfiguratorUpdatesConfigWithNilCustomIngressTemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		IngressTemplate: nil,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -159,7 +160,7 @@ func TestConfiguratorUpdatesConfigWithCustomIngressTemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		IngressTemplate: &customTestIngressTemplate,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -181,7 +182,7 @@ func TestConfigratorUpdatesConfigWithCustomVStemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		VirtualServerTemplate: &customTestVStemplate,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -203,7 +204,7 @@ func TestConfiguratorUpdatesConfigWithNilCustomVSemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		VirtualServerTemplate: nil,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -221,7 +222,7 @@ func TestConfigratorUpdatesConfigWithCustomTStemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		TransportServerTemplate: &customTestTStemplate,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -243,7 +244,7 @@ func TestConfiguratorUpdatesConfigWithNilCustomTStemplate(t *testing.T) {
 	cnf := createTestConfigurator(t)
 	warnings, err := cnf.UpdateConfig(&ConfigParams{
 		TransportServerTemplate: nil,
-	}, ExtendedResources{})
+	}, &MGMTConfigParams{}, ExtendedResources{})
 	if err != nil {
 		t.Fatal(err)
 	}
